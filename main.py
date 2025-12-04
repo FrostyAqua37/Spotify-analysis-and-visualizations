9ec5c37dafd50897da1bf3cac9d7b526a9492287
Eivind Chen <112402146+FrostyAqua37@users.noreply.github.com>
2025-12-04 22:50:53 +0100

    Cleaned and formatted several datasets for visualization and analysis.

---
+++ main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+top_tracks_2019 = pd.read_csv('Uncleaned data/Top tracks 2019.csv', encoding='ISO-8859-1', index_col=0)
+top_tracks_2020 = pd.read_csv('Uncleaned data/Top tracks 2020.csv')
+top_tracks_2021 = pd.read_csv('Uncleaned data/Top tracks 2021.csv')
+top_tracks_2022 = pd.read_csv('Uncleaned data/Top tracks 2022.csv')
+top_tracks_2023 = pd.read_csv('Uncleaned data/Top tracks 2023.csv', encoding='ISO-8859-1')
+top_tracks_2024 = pd.read_csv('Uncleaned data/Top tracks 2024.csv', encoding='ISO-8859-1')
+
+#Shortening the dataframes with many rows down to 100.
+top_tracks_2020 = top_tracks_2020.head(100)
+top_tracks_2021 = top_tracks_2021.head(100)
+top_tracks_2023 = top_tracks_2023.head(100)
+top_tracks_2024 = top_tracks_2024.head(100)
+
+#top_tracks_2019.drop_duplicates(inplace=True)
+top_tracks_2020.drop_duplicates(subset='Title', inplace=True)
+#top_tracks_2021.drop_duplicates(subset='song_name', inplace=True)
+#top_tracks_2022.drop_duplicates(inplace=True)
+#top_tracks_2023.drop_duplicates(inplace=True)
+top_tracks_2024.drop_duplicates(subset='Track', inplace=True)
+
+top_tracks_2019.drop(columns = ['Beats.Per.Minute', 'Energy', 'Danceability', 'Loudness..dB..', 
+                                'Liveness', 'Valence.', 'Length.', 'Acousticness..', 'Speechiness.'],
+                                inplace=True)
+
+top_tracks_2020.drop(columns = 'Country', inplace=True)
+
+columns_2021 = ['spotify_id', 'song_name', 'artist_name', 'song_popularity', 
+           'album_release_date', 'album_release_year', 'album_release_month']
+top_tracks_2021 = top_tracks_2021[columns_2021]
+
+columns_2022 = ['Spotify ID', 'Artist IDs', 'Track Name', 'Album Name', 'Artist Name(s)', 
+                'Release Date', 'Duration (ms)', 'Popularity', 'Added By', 'Added At',
+                'Genres']
+top_tracks_2022 = top_tracks_2022[columns_2022]
+
+columns_2023 = ['track_name', 'artist(s)_name', 'artist_count', 'released_year',
+                'released_month', 'released_day', 'in_spotify_playlists', 'in_spotify_charts',
+                'streams', 'in_apple_playlists', 'in_apple_charts']
+top_tracks_2023 = top_tracks_2023[columns_2023]
+
+
+columns_2024 = ['Track', 'Album Name', 'Artist', 'Release Date', 'All Time Rank', 'Track Score', 
+                'Spotify Streams', 'Spotify Playlist Count', 'Spotify Playlist Reach', 
+                'Spotify Popularity', 'YouTube Views', 'YouTube Likes', 'Apple Music Playlist Count']
+top_tracks_2024 = top_tracks_2024[columns_2024]
+
+#Removing any rows with missing values
+top_tracks_2019.dropna(inplace=True)
+top_tracks_2020.dropna(inplace=True)
+top_tracks_2021.dropna(inplace=True)
+top_tracks_2022.dropna(inplace=True)
+top_tracks_2023.dropna(inplace=True)
+top_tracks_2024.dropna(inplace=True)
+
+top_tracks_2019.rename(columns={'Track.Name':'track', 'Artist.Name':'artists',
+                                'Genre':'genres', 'Popularity':'popularity'}, inplace=True)
+
+top_tracks_2020.rename(columns={'Artist':'artists', 'Date':'date','Rank':'rank',
+                                'Streams':'streams', 'Title':'title'}, inplace=True)
+
+top_tracks_2021.rename(columns={'Unnamed: 0':'index', 'song_name': 'track','artist_name':'artists', 'song_popularity':'popularity'}, inplace=True)
+
+top_tracks_2022.rename(columns={'Spotify ID':'spotify_id', 'Artist IDs': 'artist_id', 'Track Name':'track',
+                                'Album Name':'album', 'Artist Name(s)':'artists', 'Release Date':'release_date',
+                                'Duration (ms)':'duration (ms)', 'Popularity':'popularity',
+                                'Added By':'added_by', 'Added At':'added_at', 'Genres':'genres'}, inplace=True)
+
+top_tracks_2023.rename(columns={'track_name':'track', 'artist(s)_name':'artists'}, inplace=True)
+
+top_tracks_2024.rename(columns={'Track':'track', 'Album Name':'album_name', 'Artist':'artists', 'Release Date':'release_date',
+                                'All Time Rank':'all_time_rank', 'Track Score':'track_score', 'Spotify Streams':'spotify_streams',
+                                'Spotify Playlist Count':'spotify_playlist_count', 'Spotify Playlist Reach':'spotify_playlist_reach',
+                                'Spotify Popularity':'spotify_popularity', 'YouTube Views':'youtube_views', 'YouTube Likes': 'youtube_likes',
+                                'Apple Music Playlist Count':'apple_music_playlist_count'}, inplace=True)
+
+#top_tracks_2024.to_csv('Cleaned data/top_tracks_2024_cleaned.csv', index=True, encoding='utf-8')
