95e0a51bc36caa156b4afaa1f2b0572bdc243722
Eivind Chen <112402146+FrostyAqua37@users.noreply.github.com>
2025-12-17 17:24:35 +0100

    Finished creating visualization for top 10 tracks of 2024.

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+import matplotlib.ticker as mtick
+
+plt.rcParams["font.sans-serif"] = ["Arial", "DejaVu Sans"]
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.size"] = 12
 
 top_tracks_2024 = pd.read_csv('Cleaned data/top_tracks_2024_cleaned.csv')
 top_10_tracks_2024 = top_tracks_2024[['artists', 'track', 'spotify_streams']].head(10)
 top_10_tracks_2024.sort_values('spotify_streams', ascending=True, inplace=True)
 
-label_customization = dict({'family':'Times New Roman', 'fontsize':18})
+label_customization = dict({'fontsize': 14, 'weight':'bold',
+                            'labelpad': 15})
 
-plt.barh(top_10_tracks_2024['track'], top_10_tracks_2024['spotify_streams'], color='#1ED760',
+fig, ax = plt.subplots(figsize=(8, 6))
+
+ax.barh(top_10_tracks_2024['track'], top_10_tracks_2024['spotify_streams'], color='#4B0082',
          edgecolor='black')
-plt.title('Top 10 songs of 2024', family='Times New Roman', fontsize=20)
-plt.xlabel('Number of Listeners', **label_customization)
-plt.ylabel('Name of songs', **label_customization)
-plt.ticklabel_format(style='plain', useOffset=False, axis='x')
-plt.grid()
 
+#Text for title, X and Y Axis.
+ax.set_title('Top 10 songs of 2024', fontsize=24, weight='bold', pad=20)
+ax.set_xlabel('Number of listeners (in billions)', **label_customization)
+ax.set_ylabel('Name of Songs', **label_customization)
 
+ax.set_xticklabels(['0', '0,5', '1', '1,5', '2', '2,5', '3', '3,5', '4'])
+
+#Removes the outer lines except the bottom one
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+#Space between axis text and chart.
+plt.tick_params(axis='both', pad=10)
+ax.grid()
+
+plt.savefig('Top 10 Tracks of 2024.jpg', bbox_inches='tight', dpi=300)
 plt.show()
