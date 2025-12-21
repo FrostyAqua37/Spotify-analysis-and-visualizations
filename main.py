e5dfd43837ff96ff30092e263ecddf340fa083f2
Eivind Chen <112402146+FrostyAqua37@users.noreply.github.com>
2025-12-21 18:55:53 +0100

    Created Visualization for Most Popular Songs of 2022.

--- main.py
+++ main.py
@@ -8,58 +8,23 @@ plt.rcParams["font.size"] = 12
 
 label_customization = dict({'fontsize': 14, 'weight':'bold'})
 
+most_popular_2022 = pd.read_csv('Cleaned data/top_tracks_2022_cleaned.csv')
 
-top_tracks_2024 = pd.read_csv('Cleaned data/top_tracks_2024_cleaned.csv')
-top_tracks_2024.sort_values('spotify_streams', ascending=True, inplace=True)
-top_10_tracks_2024 = top_tracks_2024[['artists', 'track', 'spotify_streams']].tail(10)
-
-fig, ax = plt.subplots()
-
-ax.barh(top_10_tracks_2024['track'], top_10_tracks_2024['spotify_streams'], color='#4B0082',
-         edgecolor='black')
-
-#Text for title, X and Y Axis.
-ax.set_title('Most Streamed Songs of 2024', fontsize=18, weight='bold', pad=20)
-ax.set_xlabel('Number of streams (in billions)', **label_customization)
-ax.set_ylabel('Top Tracks', **label_customization)
-
-ax.set_xticklabels(['0', '0,5', '1', '1,5', '2', '2,5', '3', '3,5', '4'])
-
-#Removes the outer lines except the bottom one
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-
-#Space between axis text and chart.
-plt.tick_params(axis='both', pad=10)
-ax.grid()
-
-plt.savefig('Visualizations/Most Streamed Songs 2024.jpg', bbox_inches='tight', dpi=300)
-
-plt.show()
-
-'''
-
-top_tracks_2023 = pd.read_csv('Cleaned data/top_tracks_2023_cleaned.csv')
-top_tracks_2023.sort_values('streams', ascending=True, inplace=True)
-top_10_tracks_2023 = top_tracks_2023[['track', 'streams']].tail(10)
+most_popular_2022.sort_values('popularity', ascending=True, inplace=True)
+popular_10_2022 = most_popular_2022[['track', 'popularity']].tail(10)
 
 fig, ax = plt.subplots()
-ax.barh(top_10_tracks_2023['track'], top_10_tracks_2023['streams'], color='#ff8c69', edgecolor='black')
 
-plt.title('Most Streamed Songs of 2023', fontsize=18, weight='bold', pad=20)
-plt.xlabel('Number of streams (in billions)', **label_customization, labelpad=15)
-plt.ylabel('Top Tracks', **label_customization, labelpad=-10)
+ax.barh(popular_10_2022['track'], popular_10_2022['popularity'], color='#CAF4F4', 
+        edgecolor='black')
+ax.set_title('Most Popular Tracks of 2022', fontsize=18, weight='bold', pad=20)
+
+ax.set_xlabel('Popularity Scale [0 – 100]', **label_customization)
+ax.set_ylabel('Name of Tracks', **label_customization)
+ax.set_xticklabels([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+ax.set_xlim((0.0, 100.0))
+
+plt.locator_params(axis='x', nbins=10)
 plt.grid()
-
-ax.set_xticklabels(['0', '0,5', '1', '1,5', '2', '2,5', '3', '3,5'])
-#Removes the outer lines except the bottom one
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-
-#Space between axis text and chart.
-plt.tick_params(axis='both', pad=10)
-
-plt.autoscale(enable=True, axis='y', tight=True)
-plt.savefig('Visualizations/Most Streamed Songs 2023.jpg', bbox_inches='tight', dpi=300)
+plt.savefig('Visualizations/Most Popular Songs 2022.jpg', bbox_inches='tight', dpi=300)
 plt.show()
-'''
